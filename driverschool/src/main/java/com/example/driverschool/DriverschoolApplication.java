4f81e3d1bfb4916022f5d23eba5c4f516903e15c
ye <767439886@qq.com>
2019-11-12 09:01:53 +0800

    1.1

---
+++ driverschool/src/main/java/com/example/driverschool/DriverschoolApplication.java
@@ -0,0 +1,13 @@
+package com.example.driverschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriverschoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DriverschoolApplication.class, args);
+    }
+
+}
---
+++ driverschool/src/test/java/com/example/driverschool/DriverschoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.driverschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriverschoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
